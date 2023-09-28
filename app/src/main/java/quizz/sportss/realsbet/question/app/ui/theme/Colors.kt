b20a0ed82f1e8ca65cdcb4005693e28454f06074
MoldOsIk <moldlegenda1337@mail.ru>
2023-09-29 00:16:39 +0300

    fix

--- app/src/main/java/quizz/sportss/realsbet/question/app/ui/theme/Colors.kt
+++ app/src/main/java/quizz/sportss/realsbet/question/app/ui/theme/Colors.kt
@@ -2,7 +2,6 @@ package quizz.sportss.realsbet.question.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val white = Color.White
 val primary = Color(0x0)
 val secondary = Color(0xFF0F2950)
 val thrid = Color(0xFF5FAE33)
